40c8a01a7352b14747bc421ebe0ec731f5f94796
Yangkai.Shen <237497819@qq.com>
2019-03-08 18:10:12 +0800

    :bug: fix swagger page 404 error

---
+++ scaffold-swagger/src/main/java/com/xkcoding/swagger/config/SwaggerBootstrapUiAutoConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Yangkai.Shen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.xkcoding.swagger.config;
+
+import com.xkcoding.launcher.constants.AppConstant;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.spring.web.SpringfoxWebMvcConfiguration;
+
+/**
+ * <p>
+ * SwaggerBootstrapUi 自动装配类
+ * </p>
+ *
+ * @package: com.xkcoding.swagger.config
+ * @description: SwaggerBootstrapUi 自动装配类
+ * @author: yangkai.shen
+ * @date: Created in 2019-03-08 17:42
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Configuration
+@Profile({AppConstant.DEV_CODE, AppConstant.TEST_CODE})
+@ConditionalOnClass(SpringfoxWebMvcConfiguration.class)
+public class SwaggerBootstrapUiAutoConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
