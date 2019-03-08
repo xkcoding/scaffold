491a691989285c2968e7f73b510e19bd046b81bc
Yangkai.Shen <237497819@qq.com>
2019-03-08 17:31:38 +0800

    :sparkles: scaffold-swagger finished

---
+++ scaffold-swagger/src/main/java/com/xkcoding/swagger/config/SwaggerProperties.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2019 Yangkai.Shen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package com.xkcoding.swagger.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * Swagger配置项
+ * </p>
+ *
+ * @package: com.xkcoding.swagger.config
+ * @description: Swagger配置项
+ * @author: yangkai.shen
+ * @date: Created in 2019-03-08 17:24
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Data
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+    /**
+     * swagger会解析的包路径，默认值：com.xkcoding
+     **/
+    private String basePackage = "com.xkcoding";
+    /**
+     * swagger会解析的url规则
+     **/
+    private List<String> basePath = new ArrayList<>();
+    /**
+     * 在basePath基础上需要排除的url规则
+     **/
+    private List<String> excludePath = new ArrayList<>();
+    /**
+     * 标题，默认值：Scaffold 接口文档系统
+     **/
+    private String title = "Scaffold 接口文档系统";
+    /**
+     * 描述，默认值：Scaffold 一款好用的第三方开发套件
+     **/
+    private String description = "Scaffold 一款好用的第三方开发套件";
+    /**
+     * 版本，默认值：0.0.1
+     **/
+    private String version = "0.0.1";
+    /**
+     * 许可证
+     **/
+    private String license = "";
+    /**
+     * 许可证URL
+     **/
+    private String licenseUrl = "";
+    /**
+     * 服务条款URL
+     **/
+    private String termsOfServiceUrl = "";
+
+    /**
+     * host信息
+     **/
+    private String host = "";
+    /**
+     * 联系人信息
+     */
+    private Contact contact = new Contact();
+    /**
+     * 全局统一鉴权配置
+     **/
+    private Authorization authorization = new Authorization();
+
+    @Data
+    @NoArgsConstructor
+    public static class Contact {
+
+        /**
+         * 联系人
+         **/
+        private String name = "Yangkai.Shen";
+        /**
+         * 联系人url
+         **/
+        private String url = "https://xkcoding.com";
+        /**
+         * 联系人email
+         **/
+        private String email = "237497819@qq.com";
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class Authorization {
+
+        /**
+         * 鉴权策略ID，需要和SecurityReferences ID保持一致
+         */
+        private String name = "";
+
+        /**
+         * 需要开启鉴权URL的正则
+         */
+        private String authRegex = "^.*$";
+
+        /**
+         * 鉴权作用域列表
+         */
+        private List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+
+        private List<String> tokenUrlList = new ArrayList<>();
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class AuthorizationScope {
+
+        /**
+         * 作用域名称
+         */
+        private String scope = "";
+
+        /**
+         * 作用域描述
+         */
+        private String description = "";
+
+    }
+}
