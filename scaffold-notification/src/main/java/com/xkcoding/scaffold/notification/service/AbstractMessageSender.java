1d3e6ec90c5d8de4b00872cc12bcc8c5cf7b32ef
Yangkai.Shen <237497819@qq.com>
2019-03-16 23:57:52 +0800

    :package: change packages.

--- scaffold-notification/src/main/java/com/xkcoding/scaffold/notification/service/AbstractMessageSender.java
+++ scaffold-notification/src/main/java/com/xkcoding/scaffold/notification/service/AbstractMessageSender.java
@@ -7,14 +7,14 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.xkcoding.notification.service;
+package com.xkcoding.scaffold.notification.service;
 
 /**
  * <p>
  * 消息发送器抽象类
  * </p>
  *
- * @package: com.xkcoding.notification.service
+ * @package: com.xkcoding.scaffold.notification.service
  * @description: 消息发送器抽象类
  * @author: yangkai.shen
  * @date: Created in 2019-03-16 23:53
